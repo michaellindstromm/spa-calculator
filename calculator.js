154b1df8e0cd2fa4d95fbc19a5c9ee36c4712a6f
Michael Lindstrom <michaellindstr@gmail.com>
2017-07-18 09:47:44 -0500

    created a calculator that works with a callback function

--- calculator.js
+++ calculator.js
@@ -1,50 +1,46 @@
 let firstNum = document.getElementById("first");
-let secondNum = document.getElementById("second");
+let secondNum = document.getElementById("second"); //Input numbers
 let adding = document.getElementById("add");
 let subtracting = document.getElementById("subtract");
 let multiplying = document.getElementById("multiply");
-let dividing = document.getElementById("divide");
-let postArea = document.getElementById("outcome");
+let dividing = document.getElementById("divide"); //Operation Buttons
+let postArea = document.getElementById("outcome"); //DOM Post area
 
 /*
   Create a function that multiplies two numbers
   passed in as arguments. Return the product.
  */
-function mulitply(a, b) {
-  let c = a * b;
-  return c;
-  console.log(c);
+function multiply(a, b) {
+  let c = Number(firstNum.value) * Number(secondNum.value);
+  postArea.innerHTML = `<p>${c}</p>`
 }
 
 /*
   Create a function that adds two numbers
   passed in as arguments. Return the sum.
  */
- function add(a, b) {
-   let c = a + b;
-   return c;
-   console.log(c);
- }
+function add(a, b) {
+  let c = Number(firstNum.value) + Number(secondNum.value);
+  postArea.innerHTML = `<p>${c}</p>`
+}
 
 /*
   Create a function that subtracts two numbers
   passed in as arguments. Return the difference.
  */
- function subtract(a, b) {
-   let c = a - b;
-   return c;
-   console.log(c);
- }
+function subtract(a, b) {
+  let c = Number(firstNum.value) - Number(secondNum.value);
+  postArea.innerHTML = `<p>${c}</p>`
+}
 
 /*
   Create a function that divides two numbers
   passed in as arguments. Return the quotient.
  */
- function divide(a, b) {
-   let c = a / b;
-   return c;
-   console.log(c);
- }
+function divide(a, b) {
+  let c = Number(firstNum.value) / Number(secondNum.value);
+  postArea.innerHTML = `<p>${c}</p>`
+}
 
 
 /*
@@ -55,13 +51,21 @@ function mulitply(a, b) {
 
   Return the value of the operation.
  */
-function doMath(a,b,func) {
-  func(a,b);
+function doMath(a, b, func) {
+  func(a, b);
 }
 
 
 
-adding.addEventListener("click", doMath(firstNum, secondNum, add));
-subtracting.addEventListener("click", doMath(firstNum, secondNum, subtract));
-multiplying.addEventListener("click", doMath(firstNum, secondNum, mulitply));
-dividing.addEventListener("click", doMath(firstNum, secondNum, divide));
+adding.addEventListener("click", function() {
+  doMath(firstNum.value, secondNum.value, add)
+});
+subtracting.addEventListener("click", function() {
+  doMath(firstNum.value, secondNum.value, subtract)
+});
+multiplying.addEventListener("click", function() {
+  doMath(firstNum.value, secondNum.value, multiply)
+});
+dividing.addEventListener("click", function() {
+  doMath(firstNum.value, secondNum.value, divide)
+});
